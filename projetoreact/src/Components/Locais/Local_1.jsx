919b51c44740d9cda3092a27122f7597f162bb35
brunopdrocha <brunopdrocha@gmail.com>
2023-11-11 18:04:12 -0300

    Implementacao prot[otipo pagina feed

---
+++ projetoreact/src/Components/Locais/Local_1.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Local_1() {
+ return (
+    <div className="carta">
+      <div className="carta-image">
+        <img src="image-url" alt="Edifício Lellis" />
+      </div>
+      <div className="carta-contentudo">
+        <h1>Edifício Lellis</h1>
+        <h2>1928 - Rua Barão de Ipanema</h2>
+        <h3>Prédio residencial mais antigo da orla de Copacabana</h3>
+        <p>
+          Construído em estilo neoclássico, em 1928, o prédio ocupa metade da
+          primeira quadra da Rua Barão de Ipanema. Ao lado foi erguido, na mesma
+          época, o Edifício São Paulo, com a fachada semelhante.
+        </p>
+        <h3>Características:</h3>
+        <ul>
+          <li>É o único prédio com apartamento térreo voltado para a praia</li>
+          <li>
+            Possui entrada através de uma porta com acesso direto à Avenida
+            Atlântica
+          </li>
+        </ul>
+      </div>
+    </div>
+ );
+}
+
+export default Local_1;
---
+++ projetoreact/src/Components/Locais/Local_2.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Local_2() {
+ return (
+    <div className="carta">
+      <div className="carta-image">
+        <img src="image-url" alt="Nome do Local 2" />
+      </div>
+      <div className="carta-contentudo">
+        <h1>Nome do Local 2</h1>
+        <h2>Data de Construção - Endereço</h2>
+        <h3>Breve descrição do local</h3>
+        <p>
+          Detalhes do local, incluindo características arquitetônicas,
+          históricas, etc.
+        </p>
+        <h3>Características:</h3>
+        <ul>
+          <li>Característica 1</li>
+          <li>Característica 2</li>
+        </ul>
+      </div>
+    </div>
+ );
+}
+
+export default Local_2;
