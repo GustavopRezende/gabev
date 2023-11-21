f324d3a4bf87a78020555a5bf3a0f8b699d7ef3b
GustavopRezende <gustavo.pessanha.rezende@outlook.com>
2023-11-21 14:44:39 -0300

    main

--- projetoreact/src/Components/MapComponent.jsx
+++ projetoreact/src/Components/MapComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet'; // Importe o Leaflet
+import L from 'leaflet';
 
-import customIcon from './—Pngtree—location vector icon_3722521.png'; // Importe a imagem personalizada
+import customIcon from './—Pngtree—location vector icon_3722521.png';
 
 const MapComponent = () => {
   const initialPosition = [-22.935361, -43.215756];
@@ -13,26 +14,35 @@ const MapComponent = () => {
     { position: [-22.925000, -43.205000], text: 'Marcador 3' },
   ];
 
-  // Crie um ícone personalizado
   const customMarkerIcon = L.icon({
     iconUrl: customIcon,
-    iconSize: [32, 32], // Defina o tamanho da imagem
-    iconAnchor: [16, 32], // Defina o ponto de ancoragem
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
   });
 
   return (
-    <MapContainer center={initialPosition} zoom={13} style={{ height: '100vh', width: '100vw' }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {markers.map((marker, index) => (
-        <Marker key={index} position={marker.position} icon={customMarkerIcon}>
-          <Popup>{marker.text}</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
+      <MapContainer center={initialPosition} zoom={13} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker.position} icon={customMarkerIcon}>
+            <Popup>{marker.text}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+      <div style={{ position: 'absolute', bottom: '20px', right: '20px', zIndex: '1000' }}>
+        <Link to="/feed">
+          <button style={{ padding: '10px', fontSize: '16px' }}>Go to Feed</button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
 export default MapComponent;
+
+
+
